64765b9387c69fddde6fca5e54127077c6dee72d
tie <tie.teiroom1234@gmail.com>
2024-11-29 16:16:31 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+
+    int i;
+    int a=0;
+    int b=0;
+    i = 1;
+    while(i != 0){
+        cout << "Enter an integer: ";
+        cin >> i;
+        if(i%2 == 0 && i != 0){
+            a = a+1;
+        }else if(i%2 == 1){
+            b = b+1;
+        }
+    }    
+    cout << "#Even numbers = "<<a<<endl;
+    cout << "#Odd numbers = "<<b;
+    
     return 0;
 }
